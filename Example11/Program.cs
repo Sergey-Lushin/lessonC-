3a1032f1d9699c21335aacf22adec7f578cfedc3
Sergey <llan@ya.ru>
2022-12-18 18:23:06 +0300

    Пример работы с массивом с N переменных

---
+++ Example11/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = { 11, 21, 18, 31, 41, 15, 61, 71, 18, 91 };
+
+int n = array.Length;
+int find = 18;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+//Console.WriteLine("Hello, World!");
